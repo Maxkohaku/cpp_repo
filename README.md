f54db553c420ce64caaffca1882798745c2cd1bc
MaxKohaku <ynhkohaku@gmail.com>
2024-11-25 22:38:14 +0800

    upload demo14

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ demo10:单例模式和线程安全
 demo11:工厂模式和抽象工厂模式
 demo12:数据结构
 demo13:排序算法
+demo14:ynhLog
---
+++ demo14/module/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include "ynhLog.hpp"
+using namespace std;
+
+
+int main(int argc, char * argv[])
+{
+    Logger::instance().setLogLevel(INFO);
+    size_t  counter = 10;
+    while (counter--){
+        logger(INFO) << string("hello wolrd!");
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+    return 0;
+}
