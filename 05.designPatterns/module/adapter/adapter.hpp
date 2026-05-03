1c8037f883b25cc0fd5cb64578a886fc5eb51824
Maxkohaku <ynhkohaku@gmail.com>
2026-05-03 16:44:46 +0800

    【update】05.designPatterns-adapter

--- 05.designPatterns/module/adapter/adapter.hpp
+++ 05.designPatterns/module/adapter/adapter.hpp
@@ -4,56 +4,68 @@
 #include <iostream>
 #include <string>
 #include <memory>
-class Mp3Player
-{
+using namespace std;
+class Mp3Player {
 public:
     Mp3Player() = default;
     ~Mp3Player() = default;
-    void playMp3(const std::string fileName)
-    {
-        std::cout << "playing file: " << fileName << std::endl;
+    void play(const string& file) {
+        cout << "play mp3 file : " << file << endl; 
     }
 };
-class AudioPlayer
-{
-public:
-    AudioPlayer() = default;
-    virtual ~AudioPlayer() = default;
-    virtual void play(const std::string& audioTyep, const std::string& fileName) = 0;
+enum class AudioType {
+    mp3,
+    mp4,
+    vlc,
+    avi
 };
-class AudioPlayerAdapter : public AudioPlayer
-{
+class AudioPlayerOld {
 public:
-    AudioPlayerAdapter() : _mp3Player(std::make_unique<Mp3Player>()) {}
+    AudioPlayerOld() = default;
+    ~AudioPlayerOld() = default;
+    void playMp4(const string& file) {
+        cout << "play mp4 file : " << file << endl; 
+    }
+    void playVlc(const string& file) {
+        cout << "play vlc file : " << file << endl; 
+    }
+};
+class AudioPlayerAbstract {
+public:
+    AudioPlayerAbstract() = default;
+    virtual ~AudioPlayerAbstract() = default;
+    virtual void play(const AudioType& type, const string& file) = 0;
+};
+class AudioPlayerAdapter : public AudioPlayerAbstract {
+public:
+    AudioPlayerAdapter() : _mp3Player(make_unique<Mp3Player>()), _audioPlayerOld(make_unique<AudioPlayerOld>()) {}
     ~AudioPlayerAdapter() = default;
-    void play(const std::string& audioTyep, const std::string& fileName) override
-    {
-        if (audioTyep == "mp3")
+    void play(const AudioType& type, const string& file) override {
+        switch (type)
         {
-            _mp3Player->playMp3(fileName);
+        case AudioType::mp3:
+            _mp3Player->play(file);
+            break;
+        case AudioType::mp4:
+            _audioPlayerOld->playMp4(file);
+            break;
+        case AudioType::vlc:
+            _audioPlayerOld->playVlc(file);
+            break;
+        default:
+            cout << "unsupported audio type" << endl;
+            break;
         }
-        else if (audioTyep == "mp4")
-        {
-            std::cout << "playing file: " << fileName << std::endl;
-        }
-        else if (audioTyep == "vlc")
-        {
-            std::cout << "playing file: " << fileName << std::endl;
-        }
-        else
-        {
-            std::cout << "unsupported audio type: " << audioTyep << std::endl;
-        }
-    }
+    } 
 private:
-    std::unique_ptr<Mp3Player> _mp3Player;
+    unique_ptr<Mp3Player> _mp3Player;
+    unique_ptr<AudioPlayerOld> _audioPlayerOld;
 };
-void adapter()
-{
-    auto player = std::make_unique<AudioPlayerAdapter>();
-    player->play("mp3", "example.mp3");
-    player->play("mp4", "example.mp4");
-    player->play("vlc", "example.vlc");
-    player->play("avi", "example.avi");
+void adapter() {
+    auto player = make_unique<AudioPlayerAdapter>();
+    player->play(AudioType::mp3, "example.mp3");
+    player->play(AudioType::mp4, "example.mp4");
+    player->play(AudioType::vlc, "example.vlc");
+    player->play(AudioType::avi, "example.avi");
 }
-#endif // _ADAPTER_HPP_
+#endif /*_ADAPTER_HPP_*/
